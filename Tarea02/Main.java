fb55e867a7e00bc8ac1796830e0f89bd3ff48192
mrtaichi <isaaniet@gmail.com>
2025-02-15 17:53:45 -0600

    feat(Tarea2): Implementa 2/3 métodos pendientes

--- Tarea02/Main.java
+++ Tarea02/Main.java
@@ -16,6 +16,40 @@
 public class Main
 {
     /*
+     * METODO AUXILIAR
+     *
+     * Obtiene el numero necesario de bits para almacenar el número especificado
+     * Utiliza Math.log para obtener el logaritmo base 2 del número, este es el indicador
+     * más rápido para obtener los bits necesarios para representar n
+     *
+     * @param n el número a trabajar
+     *
+     * @return cantidad de bits necesaria para guardar n
+     */
+    private static int getArraySize(long n){
+
+        Long N = new Long(n);
+        return (int)Math.ceil(Math.log(N.doubleValue())/Math.log(2.0));
+    }
+
+    /*
+     *  METODO AUXILIAR
+     *
+     *  Determina si el número dado es par.
+     *  Esto lo hace comprobando su bit menos significativo, si está activado
+     *  es impar.
+     *  Por lo mismo, en ciertas partes de este código se usará también para extraer
+     *  directamente el bit menos significativo del número.
+     *
+     *  @param n el número a trabajar
+     *  @return es el número par?
+     *
+     *  */
+    private static boolean isEven(long n){
+        return (n & 1) == 0;
+    }
+
+    /*
      * Convierte un entero en su representación binaria como número entero sin signo.
      * La salida de esta función debe ser un arreglo de booleanos del mínimo tamaño
      * posible para poder representar el entero, es decir que si la entrada de
@@ -27,8 +61,33 @@ public class Main
         if(n < 0) {
             throw new IllegalArgumentException("No se aceptan entradas negativas.");
         }
-        
-        return toBoolean("00000000");
+        if(n == 0){
+            return toBoolean("00000000");
+        }
+
+        boolean[] result = new boolean[getArraySize(n)];
+
+        int i = result.length-1;
+        while(n != 0){
+            result[i] = !isEven(n);
+            n/=2;
+        }
+
+        return result;
+    }
+
+    /*
+     * METODO AUXILIAR
+     *
+     * Azucar sintáctica para hacer un XOR exclusivo más legible en otras secciones
+     * del código.
+     *
+     * @param a
+     * @param b
+     * @return a XOR b
+     */
+    private static boolean XOR(boolean a, boolean b){
+        return (a || b) && (!a || !b);
     }
     
     /* 
@@ -41,16 +100,88 @@ public class Main
         if (a.length != 8 || b.length != 8) {
             throw new IllegalArgumentException("Los arreglos deben tener tamaño 8.");
         }
-        
-        boolean overflow = false;
 
-        /* Inserta tu código */
-        
+        //Usaré el boolean overflow también como espacio para carrys.
+        boolean overflow = false;
+        boolean[] result = new boolean[8];
+
+        for(int i = a.length-1; i>=0; i--){
+
+            //Aqui se suman los tres bits considerados, incluyendo el carry
+            //aunque este último no será consumido realmente hasta el siguiente paso
+            //Se usa la estructura a XOR b XOR overflow/carry
+            result[i] = XOR(XOR(a[i], b[i]), overflow);
+
+            //Si ambos bits están activados entonces habilitamos el carry
+            //de la misma manera en que si no lo están, esto emula el consumir
+            //un posible carry previo
+            //También si el overflow está cargado pero alguno de los dos bits
+            //no lo está, se considera que no se vuelve a consumir
+            overflow = a[i] && b[i] || overflow && (a[i] || b[i]);
+
+        }
+
+        //Redefiniendo overflow como lo q es originalmente
+        //
+        //De esta manera detecta si hubo cambio de signo donde no debió de haber habido.
+        //
+        //Casos de overflow:
+        //Negativo + Negativo = Positivo
+        //Positivo + Positivo = Negativo
+        overflow = a[0] && b[0] && !result[0];
+
         if (overflow) {
             throw new ArithmeticException("Overflow detectado en la suma de enteros con complemento a dos.");
         }
         
-        return toBoolean("00000000");
+        return result;
+    }
+
+    /*
+     * METODO AUXILIAR
+     *
+     * Realiza el algoritmo de suma sin más ni menos, tira la excepción overflow si
+     * se conserva el carry al finalizar el algoritmo
+     *
+     * @param a
+     * @param b
+     * @return a + b en operación unsigned
+     */
+    private static boolean[] unsignedSum(boolean[] a, boolean[] b){
+        if(a.length != b.length){
+            throw new IllegalArgumentException("Suma de numeros de longitud distinta no implementada");
+        }
+
+        boolean carry = false;
+        int longitud = a.length - 1;
+        boolean[] resultado = new boolean[longitud+1];
+
+        for(int i = longitud; i >= 0; i--){
+            //Sumador clásico
+
+            resultado[i] = XOR(XOR(a[i], b[i]), carry);
+
+            carry = (a[i] && b[i]) || carry && (a[i] || b[i]);
+        }
+
+        if(carry){
+            throw new ArithmeticException("Overflow detectado");
+        }
+
+        return resultado;
+    }
+
+    private static boolean[] getBiggerNumber(boolean[] a, boolean[] b){
+        int tamanioMasCorto = a.length >= b.length ? a.length : b.length;
+        boolean[] result = a;
+
+        for(int i = 0; i < tamanioMasCorto; i++){
+            if(a[i] != b[i]){
+                result = a[i] && !b[i] ? a : b;
+            }
+        }
+
+        return result;
     }
     
     /* 
@@ -65,14 +196,44 @@ public class Main
         }
         
         boolean overflow = false;
+        boolean signoA = a[0];
+        boolean signoB = b[0];
+        boolean[] result = new boolean[8];
 
-        /* Inserta tu código */
-        
+
+
+        if(signoA == signoB){
+            //Signos iguales entonces realizamos la suma sin bronca
+            boolean[] subA = new boolean[7];
+            boolean[] subB = new boolean[7];
+            System.arraycopy(a, 1, subA, 0, 7);
+            System.arraycopy(b, 1, subB, 0, 7);
+            System.arraycopy(unsignedSum(subA, subB),
+                             0, result, 1, 7);
+
+            //Solo hay que considerar cambiar el signo para que sea acorde
+            result[0] = signoA;
+
+        }else{
+            //Signos distintos, primero hay que determinar cuál es el número más grande y cuál el más chico, para
+            //obtener la diferencia.
+            boolean[] magnitudA = new boolean[7];
+            boolean[] magnitudB = new boolean[7];
+            System.arraycopy(a, 1, magnitudA, 0, 7);
+            System.arraycopy(b, 1, magnitudB, 0, 7);
+
+            boolean[] numeroGrande = getBiggerNumber(magnitudA, magnitudB);
+            boolean[] numeroChico = numeroGrande == magnitudA? magnitudB : magnitudA;
+
+
+
+        }
+
         if (overflow) {
             throw new ArithmeticException("Overflow detectado en la suma de enteros con bit de signo.");
         }
         
-        return toBoolean("00000000");
+        return result;
     }
     
     /* Convierte un arreglo de booleanos en una cadena de 1'0 y 0's
